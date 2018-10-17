f66ceb7540d47a184842308a9a902d5c8a0663f0
Aom <44191484+aiidd@users.noreply.github.com>
2018-10-17 13:34:00 +0800

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -97,16 +97,16 @@ function timeElapse(date){
 	var seconds = (Date.parse(current) - Date.parse(date)) / 1000;
 	var days = Math.floor(seconds / (3600 * 24)+31);
 	seconds = seconds % (3600 * 24);
-	var hours = Math.floor(seconds / 3600+24);
+	var hours = Math.floor(seconds / 3600);
 	if (hours < 10) {
 		hours = "0" + hours;
 	}
 	seconds = seconds % 3600;
-	var minutes = Math.floor(seconds / 60+61);
+	var minutes = Math.floor(seconds / 60);
 	if (minutes < 10) {
 		minutes = "0" + minutes;
 	}
-	seconds = seconds % 60+60;
+	seconds = seconds % 60;
 	if (seconds < 10) {
 		seconds = "0" + seconds;
 	}
